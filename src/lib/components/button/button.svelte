760702203e4c17799c2306a458192d9f3488e173
mufarodev <mufaro.priv@gmail.com>
2026-06-16 00:12:12 +0000

    Set up SvelteKit library foundation

---
+++ src/lib/components/button/button.svelte
@@ -0,0 +1,14 @@
+<script lang="ts">
+	import type { Snippet } from 'svelte';
+	import type { HTMLButtonAttributes } from 'svelte/elements';
+
+	type ButtonProps = HTMLButtonAttributes & {
+		children?: Snippet;
+	};
+
+	let { children, type = 'button', ...restProps }: ButtonProps = $props();
+</script>
+
+<button {type} {...restProps}>
+	{@render children?.()}
+</button>
---
+++ src/lib/index.ts
@@ -0,0 +1,2 @@
+export * from './utils';
+export { default as Button } from './components/button/button.svelte';
