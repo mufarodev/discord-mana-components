f3c2024d7a1cb1ac7c51334afb032d1ae7ca6910
mufarodev <mufaro.priv@gmail.com>
2026-06-19 00:06:57 +0000

    Add tabs component

---
+++ src/lib/components/tabs/tabs-list.svelte
@@ -0,0 +1,14 @@
+<script lang="ts">
+	import { Tabs as TabsPrimitive } from 'bits-ui';
+	import { cn } from '$lib/utils';
+
+	let { class: className, ...restProps }: TabsPrimitive.ListProps = $props();
+</script>
+
+<TabsPrimitive.List
+	class={cn(
+		'flex flex-row border-b-2 border-solid border-dm-border-subtle',
+		className
+	)}
+	{...restProps}
+/>
---
+++ src/lib/components/tabs/tabs-trigger.svelte
@@ -0,0 +1,15 @@
+<script lang="ts">
+	import { Tabs as TabsPrimitive } from 'bits-ui';
+	import { cn } from '$lib/utils';
+
+	let { class: className, value, ...restProps }: TabsPrimitive.TriggerProps = $props();
+</script>
+
+<TabsPrimitive.Trigger
+	{value}
+	class={cn(
+		'pb-4 mr-8 -mb-[2px] cursor-pointer text-[#949ba4] dark:text-[#949ba4] light:text-[#6b6e75] font-medium hover:text-dm-interactive-hover data-[state=active]:text-dm-interactive-active data-[state=active]:border-b-2 data-[state=active]:border-solid data-[state=active]:border-dm-brand transition-all duration-150 outline-none select-none focus-visible:ring-1 focus-visible:ring-dm-brand',
+		className
+	)}
+	{...restProps}
+/>
