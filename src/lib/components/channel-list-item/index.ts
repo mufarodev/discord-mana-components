f05bac59e5576d1961380938bcdd9ede471bfabe
mufarodev <mufaro.priv@gmail.com>
2026-06-19 00:09:49 +0000

    Add channel list item component

---
+++ src/lib/components/channel-list-item/index.ts
@@ -0,0 +1,8 @@
+import Root, { type ChannelListItemProps, channelVariants } from './channel-list-item.svelte';
+
+export {
+	Root,
+	Root as ChannelListItem,
+	channelVariants,
+	type ChannelListItemProps
+};
