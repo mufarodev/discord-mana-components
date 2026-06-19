c50b850c97e502c78360f37020d3061fedf19f88
mufarodev <mufaro.priv@gmail.com>
2026-06-19 00:09:09 +0000

    Add avatar component

---
+++ src/lib/components/avatar/index.ts
@@ -0,0 +1,10 @@
+import Root, { avatarVariants, type AvatarProps, type AvatarSize, type AvatarStatus } from './avatar.svelte';
+
+export {
+	Root,
+	Root as Avatar,
+	avatarVariants,
+	type AvatarProps,
+	type AvatarSize,
+	type AvatarStatus
+};
