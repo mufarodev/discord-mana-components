4e1946da98ccca8db9e2aeefada44c0eb81c85ab
mufarodev <mufaro.priv@gmail.com>
2026-06-24 12:29:36 +0000

    feat: replace `cn` function to use `cnfast`

--- src/lib/utils.ts
+++ src/lib/utils.ts
@@ -1,9 +1,6 @@
-import { clsx, type ClassValue } from "clsx";
-import { twMerge } from "tailwind-merge";
+import { cn } from "cnfast";
 
-export function cn(...inputs: ClassValue[]): string {
-  return twMerge(clsx(inputs));
-}
+export { cn };
 
 export type WithoutChild<T> = T extends { child?: unknown } ? Omit<T, "child"> : T;
 export type WithoutChildren<T> = T extends { children?: unknown } ? Omit<T, "children"> : T;
